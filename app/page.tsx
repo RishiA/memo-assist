49ddb4f0d86a47e3307f704c08a548fa8ac7a9e0
Rishi Athanikar <RishiA@users.noreply.github.com>
2024-06-21 13:46:45 -0400

    memo update

--- app/page.tsx
+++ app/page.tsx
@@ -8,27 +8,56 @@ export default function Page() {
       </h1>
       <p className="mb-4">
         
-          Justworks' mission is to help entrepreneurs and businesses grow with confidence. Until today, our PEO product has primarily enabled us to serve white-collar desk workers. But Justworks' long-term success is predicated on fulfilling our mission for _all small businesses_ regardless of their industry.
-          
-          This memo details our focus and the investments required for the next 12-18 months to enable a best-in-class State Unemployment Program that disrupts the industry and advances Justworks into a future where we support a diverse range of businesses leveraging a key strategic program: Vertical Expansion.
-          
-          These investments are key as they set the stage for our growth over the next year and a half, allowing us to uphold our mission and vision across all product lines.
-          
-          Our projections indicate that with these enhancements, Justworks can not only support **additional small businesses** within our State unemployment Insurance (SUI) program but also experience incremental, rather than linear, growth in SUI revenue. In FY25, we expect these changes to contribute **$44.2 million in** **revenue** to Justworks.
-          
-          To elevate our SUI program into a comprehensive industry-leading platform and make it a major driver of growth, we are focused on the following areas -
-          
-          1. **Strategic Underwriting:** Boosting our SUI underwriting capabilities with technology to serve a diverse portfolio of businesses.  
-          
-          2. **Optimize PEO SUI Opportunity:** Leveraging our PEO entities to create competitive SUI rates over time and improve our profit margins.
-          3. **Expand Client SUI:** Scaling up our SUI program to include all services for all customers in every state, not just PEO reporting states.  
-          
-          4. **Seamless SUI experience:** Developing comprehensive systems and streamlined processes that support our multi-product strategy, transforming the experience for both our customers and our internal teams.  
-          
-          5. **Launch SUI as a Standalone platform**: Setting up the SUI service as its own platform, starting with the basics of rate management, registration and claims management, so we can increase economies of scale through technology.  
-          
-          6. **Strategic Account Maintenance:** Enhancing our efforts with focused account maintenance to ensure we meet compliance standards and maintain the integrity of our SUI program.  
-              <br/>!
+      <p class="normal0">
+				Justworks’ mission is to help entrepreneurs and businesses grow with confidence. Until today, our PEO product has primarily enabled us to serve white-collar desk workers. But Justworks’ long-term success is predicated on fulfilling our mission for <em>all small businesses</em> regardless of their industry. 
+			</p>
+			<p class="normal0">
+				&#xa0;
+			</p>
+			<p class="normal0">
+				This memo details our focus and the investments required for the next 12-18 months to enable a best-in-class State Unemployment Program that disrupts the industry and advances Justworks into a future where we support a diverse range of businesses leveraging a key strategic program: Vertical Expansion.
+			</p>
+			<p class="normal0">
+				&#xa0;
+			</p>
+			<p class="normal0">
+				These investments are key as they set the stage for our growth over the next year and a half, allowing us to uphold our mission and vision across all product lines.&#xa0; 
+			</p>
+			<p class="normal0">
+				<strong><em>&#xa0;</em></strong>
+			</p>
+			<p class="normal0">
+				Our projections indicate that with these enhancements, Justworks can not only support <strong>additional small businesses </strong>within our State unemployment Insurance (SUI) program but also experience incremental, rather than linear, growth in SUI revenue. In FY25, we expect these changes to contribute <strong>$44.2 million in</strong> <strong>revenue</strong> to Justworks. 
+			</p>
+			<p class="normal0">
+				&#xa0;
+			</p>
+			<p class="normal0">
+				<a id="yd4bfhrp12qq"></a>To elevate our SUI program into a comprehensive industry-leading platform and&#xa0; make it a major driver of growth, we are focused on the following areas - 
+			</p>
+			<p class="normal0">
+				&#xa0;
+			</p>
+			<ol style="margin:0pt; padding-left:0pt">
+				<li class="normal0" style="margin-left:32.17pt; padding-left:3.83pt">
+					<strong>Strategic Underwriting:</strong> Boosting our SUI underwriting capabilities with technology to serve a diverse portfolio of businesses.<br />&#xa0;
+				</li>
+				<li class="normal0" style="margin-left:32.17pt; padding-left:3.83pt">
+					<strong>Optimize PEO SUI Opportunity:</strong> Leveraging our PEO entities to create competitive SUI rates over time and improve our profit margins.<br />&#xa0;
+				</li>
+				<li class="normal0" style="margin-left:32.17pt; padding-left:3.83pt">
+					<strong>Expand Client SUI: </strong>Scaling up our SUI program to include all services for all customers in every state, not just PEO reporting states.<br />&#xa0;
+				</li>
+				<li class="normal0" style="margin-left:32.17pt; padding-left:3.83pt">
+					<strong>Seamless SUI experience: </strong>Developing comprehensive systems and streamlined processes that support our multi-product strategy, transforming the experience for both our customers and our internal teams.<br />&#xa0;
+				</li>
+				<li class="normal0" style="margin-left:32.17pt; padding-left:3.83pt">
+					<strong>Launch SUI as a Standalone platform</strong>: Setting up the SUI service as its own platform, starting with the basics of rate management, registration and claims management, so we can increase economies of scale through technology.<br />&#xa0;
+				</li>
+				<li class="normal0" style="margin-left:32.17pt; padding-left:3.83pt">
+					<strong>Strategic Account Maintenance: </strong>Enhancing our efforts with focused account maintenance to ensure we meet compliance standards and maintain the integrity of our SUI program.<br /><br />
+          </li>
+        </ol>
       
             
       </p>
